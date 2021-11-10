034f871be7862ab68f47cc43799e125725c8f9ec
AVakiliT <amirvt92@gmail.com>
2021-11-10 10:38:37 -0500

    make it ranged

--- extract_url_and_timestamp_from_docnos.py
+++ extract_url_and_timestamp_from_docnos.py
@@ -1,13 +1,19 @@
 import glob
 import sys
 import pandas as pd
-
+from tqdm import trange
 
 files = sorted(glob.glob("/project/6004803/avakilit/c4_parquet/*.parquet"))
-i = int(sys.argv[1])
-df = pd.read_parquet(files[i]).set_index("docno")
+start = int(sys.argv[1])
+n = int(sys.argv[2])
 
 docnos = pd.read_csv("/project/6004803/smucker/group-data/misc/HON_Docnos.csv/part-00000-9b3df246-3321-48cc-90e2"
                      "-f0683a9c425b-c000.csv", names=["docno"], index_col="docno")
-collection_m = df.join(docnos, "docno", "inner")
-collection_m.to_parquet("Collection_M/" + files[i].split("/")[-1], index="docno")
+
+for i in trange(start * n, start * (n+1)):
+    try:
+        df = pd.read_parquet(files[i]).set_index("docno")
+    except IndexError as _:
+        continue
+    collection_m = df.join(docnos, "docno", "inner")
+    collection_m.to_parquet("Collection_M/" + files[i].split("/")[-1], index="docno")
