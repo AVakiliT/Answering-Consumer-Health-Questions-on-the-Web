6dd5e93a660811969a033d51392c340c4b615876
AVakiliT <amirvt92@gmail.com>
2021-11-10 10:55:35 -0500

    whatever

--- extract_url_and_timestamp_from_docnos.py
+++ extract_url_and_timestamp_from_docnos.py
@@ -4,13 +4,13 @@ import pandas as pd
 from tqdm import trange
 
 files = sorted(glob.glob("/project/6004803/avakilit/c4_parquet/*.parquet"))
-start = int(sys.argv[1])
 n = int(sys.argv[2])
+start = int(sys.argv[1])
 
 docnos = pd.read_csv("/project/6004803/smucker/group-data/misc/HON_Docnos.csv/part-00000-9b3df246-3321-48cc-90e2"
                      "-f0683a9c425b-c000.csv", names=["docno"], index_col="docno")
 
-for i in trange(start * n, start * (n+1)):
+for i in trange(start * n, (start+1) * n):
     try:
         df = pd.read_parquet(files[i]).set_index("docno")
     except IndexError as _:
