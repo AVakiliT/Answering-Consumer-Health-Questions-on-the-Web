5a430b356b92db339745e209d4830f7904c6bd8f
AVakiliT <amirvt92@gmail.com>
2021-11-10 00:54:12 -0500

    whatever

--- extract_url_and_timestamp_from_docnos.py
+++ extract_url_and_timestamp_from_docnos.py
@@ -10,4 +10,4 @@ df = pd.read_parquet(files[i]).set_index("docno")
 docnos = pd.read_csv("/project/6004803/smucker/group-data/misc/HON_Docnos.csv/part-00000-9b3df246-3321-48cc-90e2"
                      "-f0683a9c425b-c000.csv", names=["docno"], index_col="docno")
 collection_m = df.join(docnos, "docno", "inner")
-df.to_parquet("Collection_M/" + files[i].split("/")[-1])
+df.to_parquet("Collection_M/" + files[i].split("/")[-1], index="docno")
