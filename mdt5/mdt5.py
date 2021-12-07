821eab5483d84b6670af8459e3eef155ef87058f
AVakiliT <amirvt92@gmail.com>
2021-12-07 01:41:12 -0500

    fix mdt5

--- mdt5/mdt5.py
+++ mdt5/mdt5.py
@@ -9,12 +9,13 @@ from pygaggle.rerank.base import Query, Text
 from pygaggle.rerank.transformer import MonoT5, DuoT5
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--topic_no", default=122, required=False)
+parser.add_argument("--topic_no", default=122, required=False, type=int)
 parser.add_argument("--topic_file", default="/project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml",
                     required=False)
 parser.add_argument("--model_type", default="base", required=False)
 parser.add_argument("--bm25run",
-                    default="/project/6004803/avakilit/Trec21_Data/Top1kbm25_1p_passages/part-00000-2bef8f95-53dc-49f9-8b45-31f5deaf0be1-c000.snappy.parquet",
+                    default="/project/6004803/avakilit/Trec21_Data/Top1kbm25_1p_passages/"
+                            "part-00000-2bef8f95-53dc-49f9-8b45-31f5deaf0be1-c000.snappy.parquet",
                     required=False)
 args = parser.parse_known_args()
 
@@ -48,10 +49,11 @@ reranker = DuoT5(model=DuoT5.get_model(f"castorini/duot5-{type}-msmarco"))
 
 
 start = timer()
-reranked2 = reranker.rerank(query, top_passage_per_doc)
+reranked2 = reranker.rerank(query, top_passage_per_doc[:100])
 end = timer()
 print(f"duot5-{type}-msmarco took {end-start} seconds.")
 reranked2 = sorted(reranked2, key=lambda x: x.score, reverse=True)
 run = [(topic_no, 0, x.metadata["docid"], x.score, i + 1, type) for i, x in enumerate(reranked2)]
 
-pd.DataFrame(run)
+run_df = pd.DataFrame(run)
+run_df.to_csv(f"output/{2021 if '2021' in topic_file else 2019}-{topic_no}--{type}.run", sep=" ", index=False)
--- mdt5/mdt5.sh
+++ mdt5/mdt5.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#SBATCH --account=rrg-smucker
+#SBATCH --time=0-12:0:0
+#SBATCH --array=1-4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm/slurm-%A_%a.out
+
+#need these build environment
+#virtualenv ~/PYGAGGLE
+#source ~/PYGAGGLE/bin/activate
+#module load rust
+#module load swig
+#pip install git+https://github.com/castorini/pygaggle.git
+#pip install faiss-gpu
 
 module load python
 source ~/PYGAGGLE/bin/activate
@@ -7,8 +22,9 @@ module load java
 module load StdEnv/2020  gcc/9.3.0  cuda/11.4
 module load faiss
 module load arrow
-#need these
-#module load rust
-#module load swig
-#pip install git+https://github.com/castorini/pygaggle.git
-#pip install faiss-gpu
+
+python mdt5.py --topic_no $SLURM_ARRAY_TASK_ID \
+ --topic_file /project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml \
+ --model_type base \
+ --bm25run /project/6004803/avakilit/Trec21_Data/part-00000-2bef8f95-53dc-49f9-8b45-31f5deaf0be1-c000.snappy.parquet
+
