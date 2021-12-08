b8cfa5a0d564b2328fdd0b29cfba19bf1636fb13
AVakiliT <amirvt92@gmail.com>
2021-12-07 20:26:36 -0500

    fix mdt5

--- mdt5/mdt5.py
+++ mdt5/mdt5.py
@@ -1,6 +1,6 @@
 # input file - topic
 # output
-print("Importing...")
+print("Importing...", flush=True)
 import argparse
 from random import sample
 from timeit import default_timer as timer
@@ -10,7 +10,7 @@ import xmltodict
 from pygaggle.rerank.base import Query, Text
 from pygaggle.rerank.transformer import MonoT5, DuoT5
 
-print("Parsing args...")
+print("Parsing args...", flush=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--topic_no", default=122, required=False, type=int)
 parser.add_argument("--topic_file", default="/project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml",
@@ -25,32 +25,32 @@ args = parser.parse_known_args()
 type = args[0].model_type
 topic_no = args[0].topic_no
 topic_file = args[0].topic_file
-print("Reading Passages Dataframe...")
+print("Reading Passages Dataframe...", flush=True)
 df = pd.read_parquet(args[0].bm25run)
-print("Done.")
+print("Done.", flush=True)
 
 
-print("Loading topic file...")
+print("Loading topic file...", flush=True)
 with open(topic_file) as f:
     topics = xmltodict.parse(f.read())['topics']['topic']
 
 topic = filter(lambda x: x["number"] == str(topic_no), topics).__next__()
 query = Query(topic["query"])
 
-print("Topic query is:")
-print(topic["query"])
+print("Topic query is:", flush=True)
+print(topic["query"], flush=True)
 texts = [Text(p.passage, {'docid': p.docno}, 0) for p in
          df[df.topic == topic_no].itertuples()]
 
 # texts = sample(texts, 100)
-print("Loading MonoT5...")
+print("Loading MonoT5...", flush=True)
 reranker = MonoT5(pretrained_model_name_or_path=f"castorini/monot5-{type}-med-msmarco")
-print("Done.")
-print("Reranking with MonoT5...")
+print("Done.", flush=True)
+print("Reranking with MonoT5...", flush=True)
 start = timer()
 reranked = reranker.rerank(query, texts)
 end = timer()
-print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end-start} seconds.")
+print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end-start} seconds.", flush=True)
 reranked = sorted(reranked, key=lambda x: x.score, reverse=True)
 
 top_passage_per_doc = sorted(list({x.metadata['docid']: x for x in sorted(reranked, key=lambda i: i.score)}.values()),
@@ -58,20 +58,20 @@ top_passage_per_doc = sorted(list({x.metadata['docid']: x for x in sorted(rerank
 
 del reranked
 
-print("Loading DuoT5...")
+print("Loading DuoT5...", flush=True)
 reranker = DuoT5(model=DuoT5.get_model(f"castorini/duot5-{type}-msmarco"))
-print("Done.")
+print("Done.", flush=True)
 
-print("Reranking with DuoT5...")
+print("Reranking with DuoT5...", flush=True)
 start = timer()
 reranked2 = reranker.rerank(query, top_passage_per_doc)
 end = timer()
 
-print(f"Done. Reranking {len(top_passage_per_doc)} with duot5-{type}-msmarco took {end-start} seconds.")
+print(f"Done. Reranking {len(top_passage_per_doc)} with duot5-{type}-msmarco took {end-start} seconds.", flush=True)
 reranked2 = sorted(reranked2, key=lambda x: x.score, reverse=True)
 run = [(topic_no, 0, x.metadata["docid"], x.score, i + 1, type) for i, x in enumerate(reranked2)]
 
-print("Writing Run file...")
+print("Writing Run file...", flush=True)
 run_df = pd.DataFrame(run)
 run_df.to_csv(f"output/mdt5-topic-{2021 if '2021' in topic_file else 2019}-{topic_no}-{type}.run", sep=" ", index=False)
-print("Done.")
+print("Done.", flush=True)
