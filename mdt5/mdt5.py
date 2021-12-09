a4dad9c20aa82e7793a908e0f2d00fae9125c3cf
AVakiliT <amirvt92@gmail.com>
2021-12-09 13:21:41 -0500

    optional duo

--- mdt5/mdt5.py
+++ mdt5/mdt5.py
@@ -1,5 +1,7 @@
 # input file - topic
 # output
+import os
+
 print("Importing...", flush=True)
 import argparse
 from random import sample
@@ -12,13 +14,16 @@ from pygaggle.rerank.transformer import MonoT5, DuoT5
 
 print("Parsing args...", flush=True)
 parser = argparse.ArgumentParser()
-parser.add_argument("--topic_no", default=1, required=False, type=int)
-parser.add_argument("--topic_file", default="/project/6004803/smucker/group-data/topics/2019topics.xml",
-                    required=False)
-parser.add_argument("--model_type", default="base", required=False)
+parser.add_argument("--topic_no", default=1, type=int)
+parser.add_argument("--topic_file", default="/project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml")
+parser.add_argument("--model_type", default="3b")
 parser.add_argument("--bm25run",
-                    default="/project/6004803/avakilit/Trec21_Data/Top1kBM25_2019_1p_passages/part-00000-a697cfb9-9405-449d-8548-e4ddc6ca9f7a-c000.snappy.parquet",
-                    required=False)
+                    default="/project/6004803/avakilit/Trec21_Data/Top1kBM25_1p_passages/part-00000-0da9fef6-fd3a-48a8-96d8-f05f4d9e9da2-c000.snappy.parquet")
+
+parser.add_argument('--duo', dest='feature', action='store_true')
+parser.add_argument('--no-duo', dest='feature', action='store_false')
+parser.set_defaults(feature=True)
+
 args = parser.parse_known_args()
 
 type = args[0].model_type
@@ -26,8 +31,8 @@ topic_no = args[0].topic_no
 topic_file = args[0].topic_file
 print("Reading Passages Dataframe...", flush=True)
 df = pd.read_parquet(args[0].bm25run)
+duo = args[0].duo
 print("Done.", flush=True)
-
 
 print("Loading topic file...", flush=True)
 with open(topic_file) as f:
@@ -49,28 +54,37 @@ print("Reranking with MonoT5...", flush=True)
 start = timer()
 reranked = reranker.rerank(query, texts)
 end = timer()
-print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end-start} seconds.", flush=True)
+print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end - start} seconds.", flush=True)
 reranked = sorted(reranked, key=lambda x: x.score, reverse=True)
 
 top_passage_per_doc = sorted(list({x.metadata['docid']: x for x in sorted(reranked, key=lambda i: i.score)}.values()),
                              key=lambda i: i.score, reverse=True)
 
-del reranked
+# del reranked
 
-print("Loading DuoT5...", flush=True)
-reranker = DuoT5(model=DuoT5.get_model(f"castorini/duot5-{type}-msmarco"))
-print("Done.", flush=True)
 
-print("Reranking with DuoT5...", flush=True)
-start = timer()
-reranked2 = reranker.rerank(query, top_passage_per_doc)
-end = timer()
-print(f"Done. Reranking {len(top_passage_per_doc)} with duot5-{type}-msmarco took {end-start} seconds.", flush=True)
+if duo:
+    print("Loading DuoT5...", flush=True)
+    reranker = DuoT5(model=DuoT5.get_model(f"castorini/duot5-{type}-msmarco"))
+    print("Done.", flush=True)
+    print("Reranking with DuoT5...", flush=True)
+    start = timer()
+    top_passage_per_doc = reranker.rerank(query, top_passage_per_doc)
+    end = timer()
+    print(f"Done. Reranking {len(top_passage_per_doc)} with duot5-{type}-msmarco took {end - start} seconds.",
+          flush=True)
 
-reranked2 = sorted(reranked2, key=lambda x: x.score, reverse=True)
-run = [(topic_no, 0, x.metadata["docid"], x.score, i + 1, type) for i, x in enumerate(reranked2)]
+top_passage_per_doc = sorted(top_passage_per_doc, key=lambda x: x.score, reverse=True)
+run = [(topic_no, 0, x.metadata["docid"], i + 1, x.score, type) for i, x in enumerate(top_passage_per_doc)]
+
+output_dir = f"output_{2021 if '2021' in topic_file else '2019'}_{type}"
+try:
+    os.mkdir(output_dir)
+except FileExistsError:
+    pass
 
 print("Writing Run file...", flush=True)
 run_df = pd.DataFrame(run)
-run_df.to_csv(f"output_{2021 if '2021' in topic_file else '2019'}/mdt5-topic-{2021 if '2021' in topic_file else 2019}-{topic_no}-{type}.run", sep=" ", index=False)
+run_df.to_csv(f"{output_dir}/mdt5-topic-{2021 if '2021' in topic_file else 2019}-{topic_no}-{type}.run", sep=" ",
+              index=False, header=False)
 print("Done.", flush=True)
--- mdt5/mdt5.sh
+++ mdt5/mdt5.sh
@@ -27,13 +27,14 @@ module load java
 
 echo "Starting script..."
 
-#~/PYGAGGLE/bin/python mdt5.py --topic_no $SLURM_ARRAY_TASK_ID \
-# --topic_file /project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml \
-# --model_type base \
-# --bm25run /project/6004803/avakilit/Trec21_Data/Top1kBM25_1p_passages/part-00000-0da9fef6-fd3a-48a8-96d8-f05f4d9e9da2-c000.snappy.parquet
-
 ~/PYGAGGLE/bin/python mdt5.py --topic_no $SLURM_ARRAY_TASK_ID \
- --topic_file /project/6004803/smucker/group-data/topics/2019topics.xml \
+ --topic_file /project/6004803/smucker/group-data/topics/misinfo-2021-topics.xml \
  --model_type base \
- --bm25run /project/6004803/avakilit/Trec21_Data/Top1kBM25_2019_1p_passages/part-00000-a697cfb9-9405-449d-8548-e4ddc6ca9f7a-c000.snappy.parquet
+ --no-duo \
+ --bm25run /project/6004803/avakilit/Trec21_Data/Top1kBM25_1p_passages/part-00000-0da9fef6-fd3a-48a8-96d8-f05f4d9e9da2-c000.snappy.parquet
+
+#~/PYGAGGLE/bin/python mdt5.py --topic_no $SLURM_ARRAY_TASK_ID \
+# --topic_file /project/6004803/smucker/group-data/topics/2019topics.xml \
+# --model_type base \
+# --bm25run /project/6004803/avakilit/Trec21_Data/Top1kBM25_2019_1p_passages/part-00000-a697cfb9-9405-449d-8548-e4ddc6ca9f7a-c000.snappy.parquet
 
