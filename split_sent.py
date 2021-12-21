0658a44fd6c540920e5c44fa34c6920fe514b1b3
AVakiliT <amirvt92@gmail.com>
2021-12-21 12:03:34 -0500

    e-collection

--- split_sent.py
+++ split_sent.py
@@ -22,7 +22,7 @@ nlp.add_pipe("sentencizer")
 
 
 def lol(s):
-    # s = re.sub('\s+', " ", s.strip())
+    s = re.sub('\s+', " ", s.strip())
     seq = [sent.sent.text.strip() for sent in nlp(s).sents if len(sent) > 3]
     if len(seq) <= window_size:
         return [s]
