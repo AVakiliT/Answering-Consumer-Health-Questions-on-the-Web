693d51c31c797a00c50af43f4fbe85aa2cbbeb51
AVakiliT <amirvt92@gmail.com>
2021-12-21 13:29:37 -0500

    e-collection

--- gettop1kdocs.py
+++ gettop1kdocs.py
@@ -7,16 +7,19 @@ files = sorted(glob.glob("/project/6004803/smucker/group-data/c4_parquet/*.parqu
 n = int(sys.argv[2])
 start = int(sys.argv[1])
 output_dir = sys.argv[3]
+print(output_dir)
 docnos = pd.read_csv(sys.argv[4],
                       names="topic iter docno ranks score tag".split(), index_col="docno", sep=" ")
 
 docnos =docnos[docnos.ranks <= 1000][["topic", "score"]]
-
+print(docnos.count())
 
 for i in trange(start * n, (start+1) * n):
     try:
         df = pd.read_parquet(files[i]).set_index("docno")
+        print(df.count())
     except IndexError as _:
         continue
     collection_m = df.join(docnos, "docno", "inner")
+    print(collection_m.count())
     collection_m.to_parquet(output_dir + "/" + files[i].split("/")[-1], index="docno")
