a94d185d37a3ef6e71bd08bf2647f4cc8001f248
AVakiliT <amirvt92@gmail.com>
2021-12-28 14:33:05 -0500

    256l

--- split_sent.py
+++ split_sent.py
@@ -7,6 +7,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import *
 import pyspark.sql.functions as f
+from spacy.tokens import Span
+from typing import List
 
 window_size, step = 256, 128
 
@@ -21,15 +23,25 @@ nlp = spacy.blank("en")
 nlp.add_pipe("sentencizer")
 
 
-def lol(s):
+def tokenize_windows(s):
     s = re.sub('\s+', " ", s.strip())
-    seq = [sent.sent.text.strip() for sent in nlp(s).sents if len(sent) > 3]
-    if len(seq) <= window_size:
+    doc = nlp(s)
+    sentences = [sent.sent.text.strip() for sent in doc.sents if len(sent) > 5]
+    tokens = nlp(' '.join(sentences))
+
+    if len(tokens) <= window_size:
+        return tokens.text.strip()
+    return [' '.join(tokens[i: i + window_size]) for i in range(0, len(tokens), step)]
+
+
+def sentencize(s):
+    s = re.sub('\s+', " ", s.strip())
+    sentences = [sent.sent.text.strip() for sent in nlp(s).sents if len(sent) > 3]
+    if len(sentences) <= window_size:
         return [s]
-    return [' '.join(seq[i: i + window_size]) for i in range(0, len(seq), step)]
+    return [' '.join(sentences[i: i + window_size]) for i in range(0, len(sentences), step)]
 
-
-lol_udf = udf(lol, schema)
+lol_udf = udf(tokenize_windows, schema)
 
 df_new = df.withColumn("passage", lol_udf("text")).selectExpr("docno", "topic", "score as bm25",
                                                               "explode(passage) as passage")
