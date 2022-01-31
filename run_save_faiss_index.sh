6c135434f8ac05771e270e4ad55ddee1742affcd
Amir Vakili <amirvt92@gmail.com>
2022-01-30 19:16:46 -0500

    stuff

--- run_save_faiss_index.sh
+++ run_save_faiss_index.sh
@@ -1,11 +1,11 @@
 #SBATCH --account=rrg-smucker
-#SBATCH --time=0-03:00:00
+#SBATCH --time=0-05:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:v1001:2
 #SBATCH --output=slurm/%A_%a.out
 
-module load gcc/9.3.0 arrow scipy-stack
+module load StdEnv/2020  gcc/9.3.0  cuda/11.4 faiss arrow scipy-stack
 source ~/avakilit/PYTORCH/bin/activate
 
 python data/process.py
