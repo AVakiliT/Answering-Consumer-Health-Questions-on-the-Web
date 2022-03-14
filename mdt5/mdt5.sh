c52dde1dfa8d2f0a2d2afd94acb7e26e7914a6f9
Amir Vakili <amirvt92@gmail.com>
2022-03-14 12:48:59 -0400

    stuff

--- mdt5/mdt5.sh
+++ mdt5/mdt5.sh
@@ -2,7 +2,8 @@
 #SBATCH --account=rrg-smucker
 #SBATCH --time=0-6:0:0
 # --array=101-150
-#SBATCH --array=1-51
+#SBATCH --array=3,10,14,17,18,19,22,23,24,25,33,34,35,36,38,39,43,44,47,48,50,51
+# --array=128,129,130,131,132,133,134,135,136,150,121,122,123,124,125,126,127
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=24G
 #SBATCH --gres=gpu:v100l:1
