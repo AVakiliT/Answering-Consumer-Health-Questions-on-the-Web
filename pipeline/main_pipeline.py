0719cee634f55afc3de8eaa89e318d51ad25bd28
Amir Vakili <amirvt92@gmail.com>
2022-03-23 12:55:52 -0400

    stuff

--- pipeline/main_pipeline.py
+++ pipeline/main_pipeline.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
 
     )
 
-    data_module.setup()
+    # data_module.setup()
 
 
     lightning_module = PipelineModule(
@@ -143,7 +143,8 @@ if __name__ == '__main__':
     #
     # # fit trainer
     # lightning_module.fix_stupid_metric_device_bs()
-    trainer.fit(lightning_module, data_module)
+    # trainer.fit(lightning_module, data_module)
+    trainer.validate(lightning_module, data_module)
 
 
 
