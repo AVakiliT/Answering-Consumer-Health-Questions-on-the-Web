b6c8c3ed24c7ad6cdabbbb90b21e692435bb83ac
Amir Vakili <amirvt92@gmail.com>
2022-04-28 15:56:51 -0400

    stuff

---
+++ qreldataset/mt5.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --account=rrg-smucker
+#SBATCH --time=0-3:0:0
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:1
+
+
+module load scipy-stack/2022a gcc arrow cuda/11
+source ~/PYTORCH/bin/activate
+
+ipython ./qreldataset/mt5.py
