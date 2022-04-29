d163d34d78243064c2d259b41c0d9d926403ba6d
Amir Vakili <amirvt92@gmail.com>
2022-04-29 00:18:05 -0400

    stuff

--- qreldataset/mt5.py
+++ qreldataset/mt5.py
@@ -9,7 +9,10 @@ from transformers import T5ForConditionalGeneration, AutoModelForSeq2SeqLM
 import os
 
 from torch.cuda import amp
-from mt5lib import Query, Text, MonoT5
+try:
+    from mt5lib import Query, Text, MonoT5
+except:
+    from qreldataset.mt5lib import Query, Text, MonoT5
 print("Importing...", flush=True)
 import argparse
 from random import sample
@@ -23,7 +26,7 @@ df = df[df.text.apply(len).gt(0)]
 topics = pd.read_csv("./data/topics.csv", sep="\t")
 df = df.merge(topics["topic description efficacy".split()], on="topic", how="inner")
 runs = []
-for topic in tqdm(df.topic.unique().tolist()):
+for topic in tqdm(df.topic.unique().tolist()[:2]):
 
     # df = df[df.topic == topic].merge(topics[topics.topic == topic]["topic description efficacy".split()], on="topic", how="inner")
     query = Query(topics[topics.topic == topic].iloc[0].description)
@@ -33,21 +36,21 @@ for topic in tqdm(df.topic.unique().tolist()):
 
     reranker = MonoT5(pretrained_model_name_or_path=f"castorini/monot5-base-med-msmarco")
 
-    print("Reranking with MonoT5...", flush=True)
+    # print("Reranking with MonoT5...", flush=True)
     start = timer()
     with amp.autocast():
         reranked = reranker.rerank(query, texts)
     end = timer()
-    print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end - start} seconds.", flush=True)
+    # print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end - start} seconds.", flush=True)
 
     top_passage_per_doc = sorted(list(
         {x.metadata['metadata']: x for x in sorted(reranked, key=lambda i: i.score)}
             .values()),
         key=lambda i: i.score, reverse=True)
 
-    run = [(topic, 0,  i + 1, x.score, *x.metadata["metadata"][1:]) for i, x in enumerate(top_passage_per_doc)]
+    run = [(topic, 0,  i + 1, x.score, *x.metadata["metadata"][1:], x.text) for i, x in enumerate(top_passage_per_doc)]
 
-    run_df = pd.DataFrame(run, columns="topic iter rank score docno usefulness credibility stance url description efficacy".split())
+    run_df = pd.DataFrame(run, columns="topic iter rank score docno usefulness credibility stance url description efficacy text".split())
     runs.append(run_df)
 
 final_df = pd.concat(runs)
