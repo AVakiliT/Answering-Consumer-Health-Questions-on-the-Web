a1d1a493486bc2a918e800f57b1bca59f4e6baed
Amir Vakili <amirvt92@gmail.com>
2022-05-02 23:26:48 -0400

    stuff

--- boolq/boolq-qrels.py
+++ boolq/boolq-qrels.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from pathlib import Path
 from typing import Optional, Any
 
 import numpy as np
@@ -162,7 +163,9 @@ if __name__ == '__main__':
             print(len(self.val_outs))
             prediction = np.hstack([output['prediction'] for output in self.val_outs])
             target = np.hstack([output['target'] for output in self.val_outs])
-            with open(CHECKPOINT_PATH + f"/metrics/epoch-{self.current_epoch}.txt", 'w') as f:
+            path = Path(CHECKPOINT_PATH + f"/metrics/epoch-{self.current_epoch}.txt")
+            path.mkdir(parents=True, exist_ok=True)
+            with open(path, 'w') as f:
                 print(classification_report(target, prediction, zero_division=1), file=f)
             self.current_epoch += 1
 
