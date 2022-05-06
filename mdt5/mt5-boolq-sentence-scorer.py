13cc5d340a383ee661807d7b7a1981ae7341e8e3
Amir Vakili <amirvt92@gmail.com>
2022-05-06 10:14:57 -0400

    stuff

--- mdt5/mt5-boolq-sentence-scorer.py
+++ mdt5/mt5-boolq-sentence-scorer.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 import pandas as pd
 import torch
@@ -69,4 +70,7 @@ for batch in tqdm(data_loader):
 # embeddings = torch.cat(embeddings)
 probs = torch.cat(probs)
 # torch.save(embeddings, "gnn_fraud/embeddings_2019.pt")
-df["probs"] = probs
+df["probs"] = pd.Series([x.numpy() for x in probs])
+out_path = f"./data/{args[0].bm25run}_1p_sentences_with_probs/topic-{topic_no}.snappy.parquet"
+Path(out_path).parent.mkdir(exist_ok=True)
+df.to_parquet(out_path)
