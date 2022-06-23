f949e955611004326fa2a0186da323d8eccb17df
Amir Vakili <amirvt92@gmail.com>
2022-06-23 00:38:13 -0400

    stuff

--- qreldataset/mt5-runs.py
+++ qreldataset/mt5-runs.py
@@ -2,6 +2,9 @@
 # output
 
 from torch.cuda import amp
+from itertools import chain
+
+from tqdm import tqdm
 
 try:
     from mt5lib import Query, Text, MonoT5
@@ -12,40 +15,44 @@ import argparse
 from timeit import default_timer as timer
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--topic", default=101, type=int)
+parser.add_argument("--n", type=int)
+parser.add_argument("--k", type=int, default=25)
 args = parser.parse_known_args()
-topic = args[0].topic
+k = args[0].k
+n = args[0].n
 # topic = 1001
-
+topics = list(chain(range(1, 51 + 1), range(101, 150 + 1), range(1001, 1090 + 1)))
+topics_subset = topics[n * k: n * k + k]
 import pandas as pd
 
-window, step = 12,6
+window, step = 12, 6
 # df = pd.read_parquet("qreldataset/2019qrels.passages.parquet")
-df = pd.read_parquet(f"data/RunBM25.1k.passages_{window}_{step}/")
-df = df[df.topic.eq(topic)]
-topics = pd.read_csv("./data/topics_fixed_extended.tsv.txt", sep="\t")
-df = df.merge(topics["topic description efficacy".split()], on="topic", how="inner")
+df_all = pd.read_parquet(f"data/RunBM25.1k.passages_{window}_{step}/")
+for topic in tqdm():
+    df = df_all[df_all.topic.eq(topic)]
+    topics = pd.read_csv("./data/topics_fixed_extended.tsv.txt", sep="\t")
+    df = df.merge(topics["topic description efficacy".split()], on="topic", how="inner")
 
-# df = df[df.topic == topic].merge(topics[topics.topic == topic]["topic description efficacy".split()], on="topic", how="inner")
-query = Query(topics[topics.topic == topic].iloc[0].description)
+    # df = df[df.topic == topic].merge(topics[topics.topic == topic]["topic description efficacy".split()], on="topic", how="inner")
+    query = Query(topics[topics.topic == topic].iloc[0].description)
 
-texts = [Text(p[1].passage, p[1]) for p in
-         df.iterrows()]
+    texts = [Text(p[1].passage, p[1]) for p in
+             df.iterrows()]
 
-reranker = MonoT5(pretrained_model_name_or_path=f"castorini/monot5-base-med-msmarco")
+    reranker = MonoT5(pretrained_model_name_or_path=f"castorini/monot5-base-med-msmarco")
 
-# print("Reranking with MonoT5...", flush=True)
-start = timer()
-with amp.autocast():
-    reranked = reranker.rerank(query, texts)
-end = timer()
-# print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end - start} seconds.", flush=True)
+    # print("Reranking with MonoT5...", flush=True)
+    start = timer()
+    with amp.autocast():
+        reranked = reranker.rerank(query, texts)
+    end = timer()
+    # print(f"Done. reraking {len(texts)} passages with monot5-{type}-med-msmarco took {end - start} seconds.", flush=True)
 
-top_passage_per_doc = {x.metadata["docno"]: (x, x.score) for x in sorted(reranked, key=lambda x: x.score)}
+    top_passage_per_doc = {x.metadata["docno"]: (x, x.score) for x in sorted(reranked, key=lambda x: x.score)}
 
-run = [{"ranking": i + 1, "score": x[1], **x[0].metadata.to_dict()} for i, x in enumerate(
-    sorted(top_passage_per_doc.values(), key=lambda x: x[1], reverse=True))]
+    run = [{"ranking": i + 1, "score": x[1], **x[0].metadata.to_dict()} for i, x in enumerate(
+        sorted(top_passage_per_doc.values(), key=lambda x: x[1], reverse=True))]
 
-run_df = pd.DataFrame(run)
+    run_df = pd.DataFrame(run)
 
-run_df.to_parquet(f"data/RunBM25.1k.passages_{window}_{step}.top_mt5/{topic}.snappy.parquet")
+    run_df.to_parquet(f"data/RunBM25.1k.passages_{window}_{step}.top_mt5/{topic}.snappy.parquet")
--- qreldataset/mt5-runs.sh
+++ qreldataset/mt5-runs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=rrg-smucker
-#SBATCH --time=0-0:30:0
-#SBATCH --array=1-51,101-150,1001-1090
+#SBATCH --time=0-1:00:0
+#SBATCH --array=0-7
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
 #SBATCH --gres=gpu:v100l:1
@@ -13,6 +13,6 @@ source ~/avakilit/PYTORCH/bin/activate
 
 echo "Starting script..."
 
-ipython --ipython-dir=/tmp qreldataset/mt5-runs.py -- --topic $SLURM_ARRAY_TASK_ID
+ipython --ipython-dir=/tmp qreldataset/mt5-runs.py -- --n $SLURM_ARRAY_TASK_ID
 
 
