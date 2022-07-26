18d66dbfef4010985fc293880789f5dcf4364d80
Amir Vakili <amirvt92@gmail.com>
2022-07-26 18:25:46 -0400

    stuff

--- qreldataset/mt5lib.py
+++ qreldataset/mt5lib.py
@@ -336,7 +336,7 @@ class MonoT5(Reranker):
 
     @staticmethod
     def get_tokenizer(pretrained_model_name_or_path: str,
-                      *args, batch_size: int = 128, **kwargs) -> T5BatchTokenizer:
+                      *args, batch_size: int = 512, **kwargs) -> T5BatchTokenizer:
         return T5BatchTokenizer(
             AutoTokenizer.from_pretrained(pretrained_model_name_or_path, use_fast=False, *args, **kwargs),
             batch_size=batch_size
